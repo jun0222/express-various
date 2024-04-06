f5d519cb177196706cadc17577a5f3ef3d0ca2b9
JunHatanaka <nextseason6@gmail.com>
2024-04-07 02:28:11 +0900

    feat: アクセスログ出力

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,13 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import schema from "../schema";
 import resolvers from "../resolvers";
+import morgan from "morgan";
 
 const app = express();
 const port = 4000;
+
+// morganを使用してリクエストログを出力
+app.use(morgan("dev"));
 
 app.use(
   "/graphql",
